7c1c418d609f5a6e691ad84a6e8886d81285d2de
Héctor Plaza García <hector.plaza@lognext.com>
2026-04-08 09:08:34 +0200

    favicons en todas las html + traducciones JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
   const video = document.getElementById('heroVideo');
   const playPauseBtn = document.getElementById('videoPlayPauseBtn');
 
-  // Autoplay para que funcione 
+
   if (video) {
     video.play().catch(error => {
       console.log('Autoplay blocked:', error);
     });
   }
 
-  // boton play/pause
   if (playPauseBtn && video) {
     playPauseBtn.addEventListener('click', function() {
       if (video.paused) {
